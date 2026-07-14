4db0bc2bfe5ebed7ae4de8f939fbe77b3042db48
JP <jpe911@gmail.com>
2026-07-14 07:12:30 -0400

    Add main() and operate() helper functions

--- JS101-Programming-Foundations/Lesson_2/calculator.js
+++ JS101-Programming-Foundations/Lesson_2/calculator.js
@@ -16,51 +16,56 @@ function invalidNumber(number) {
   return number.trimStart() === ''  || Number.isNaN(Number(number));
 }
 
+function operate(operation, number1, number2) {
+  let result = null;
+
+  switch (operation) {
+    case "1":
+      result = Number(number1) + Number(number2);
+      console.log(`The result is ${result}`);
+      break;
+    case "2":
+      result = Number(number1) - Number(number2);
+      console.log(`The result is ${result}`);
+      break;
+    case "3":
+      result = Number(number1) * Number(number2);
+      console.log(`The result is ${result}`);
+      break;
+    default:
+      result = number1 / number2;
+      console.log(`The result is ${result}`);
+      break;
+  };
+};
+
+function main() {
+  prompt("What's the first number?");
+  let number1 = readline.question();
+
+  while (invalidNumber(number1)) {
+    prompt("Invalid Number. Please enter a valid number");
+    number1 = readline.question();
+  }
+
+
+  prompt("What's the second number?");
+  let number2 = readline.question();
+
+  while (invalidNumber(number2)) {
+    prompt("Invalid Number. Please enter a valid number");
+    number2 = readline.question();
+  }
+
+  prompt(`Pick the operation: \n1) Add \n2) Substract \n3) Multiply \n4) Divide`);
+  let operation = readline.question();
+
+  while (!['1', '2', '3', '4'].includes(operation)) {
+    prompt("Please choose 1, 2, 3 or 4");
+    operation = readline.question();
+  }
+  operate(operation, number1, number2);
+};
+
 console.log("Welcome to Calculator!");
-
-prompt("What's the first number?");
-let number1 = readline.question();
-
-while (invalidNumber(number1)) {
-  prompt("Invalid Number. Please enter a valid number");
-  number1 = readline.question();
-}
-
-
-prompt("What's the second number?");
-let number2 = readline.question();
-
-while (invalidNumber(number2)) {
-  prompt("Invalid Number. Please enter a valid number");
-  number2 = readline.question();
-}
-
-prompt(`Pick the operation: \n1) Add \n2) Substract \n3) Multiply \n4) Divide`);
-let operation = readline.question();
-
-while (!['1', '2', '3', '4'].includes(operation)) {
-  prompt("Please choose 1, 2, 3 or 4");
-  operation = readline.question();
-}
-
-let result = null;
-
-switch (operation) {
-  case "1":
-    result = Number(number1) + Number(number2);
-    console.log(`The result is ${result}`);
-    break;
-  case "2":
-    result = Number(number1) - Number(number2);
-    console.log(`The result is ${result}`);
-    break;
-  case "3":
-    result = Number(number1) * Number(number2);
-    console.log(`The result is ${result}`);
-    break;
-  default:
-    result = number1 / number2;
-    console.log(`The result is ${result}`);
-    break;
-}
-
+main();
