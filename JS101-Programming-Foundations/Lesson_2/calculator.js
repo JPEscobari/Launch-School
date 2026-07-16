40b540b62ad1e6130a9c7cfcf0a9b1d80c995ffa
JPEscobari <jpe911@gmail.com>
2026-07-16 13:47:07 -0400

    Add Spanish support

--- JS101-Programming-Foundations/Lesson_2/calculator.js
+++ JS101-Programming-Foundations/Lesson_2/calculator.js
@@ -23,56 +23,61 @@ function operate(operation, number1, number2) {
   switch (operation) {
     case "1":
       result = Number(number1) + Number(number2);
-      console.log(`The result is ${result}`);
+      console.log(`${number1} + ${number2} = ${result}`);
       break;
     case "2":
       result = Number(number1) - Number(number2);
-      console.log(`The result is ${result}`);
+      console.log(`${number1} - ${number2} = ${result}`);
       break;
     case "3":
       result = Number(number1) * Number(number2);
-      console.log(`The result is ${result}`);
+      console.log(`${number1} * ${number2} = ${result}`);
       break;
     default:
       result = number1 / number2;
-      console.log(`The result is ${result}`);
+      console.log(`${number1} / ${number2} = ${result}`);
       break;
   };
 };
 
-function main() {
-  prompt(message.message_1);
+function main(lang = "EN") {
+  prompt(message[lang].message_1);
   let number1 = readline.question();
 
   while (invalidNumber(number1)) {
-    prompt(message.message_invalid);
+    prompt(message[lang].message_invalid);
     number1 = readline.question();
   }
 
 
-  prompt(message.message_2);
+  prompt(message[lang].message_2);
   let number2 = readline.question();
 
   while (invalidNumber(number2)) {
-    prompt(message.message_invalid);
+    prompt(message[lang].message_invalid);
     number2 = readline.question();
   }
 
-  prompt(message.operation_message);
+  prompt(message[lang].operation_message);
   let operation = readline.question();
 
   while (!['1', '2', '3', '4'].includes(operation)) {
-    prompt(message.operation_error);
+    prompt(message[lang].operation_error);
     operation = readline.question();
   }
   operate(operation, number1, number2);
 };
 
-console.log(message.welcome);
+let lang = "EN";
+// Welcome Messages.
+console.log(message[lang].welcome);
+console.log("Please Pick a Language.\n'ES': Español.\n'EN': English");
+lang = readline.question();
 
+// Calculator's Main Loop:
 while (true) {
-  main();
-  prompt(message.continue);
+  main(lang);
+  prompt(message[lang].continue);
   let option = readline.question();
   if (option.toUpperCase() === 'Y') {
     continue;
