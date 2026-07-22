0ddaca9b7c854c44e4de014801445e87414a28e6
JP <jpe911@gmail.com>
2026-07-22 19:01:16 -0400

    Finish Medium Question 2

--- JS101-Programming-Foundations/Lesson_3/practice_problems_med1.js
+++ JS101-Programming-Foundations/Lesson_3/practice_problems_med1.js
@@ -7,4 +7,41 @@ function question (questionNumber) {
 }
 
 question(1);
-//
+// write a program that outputs The Flintstones Rock! 10 times,
+// with each line indented 1 space to the right of the line above it.
+
+function flintsPrinter(str) {
+  let padding = ' ';
+  for (let i = 0; i <= 10; i++) {
+    let printOut = `${padding.repeat(i)} ${str}`;
+    console.log(printOut);
+  }
+}
+
+flintsPrinter("The Flinstones Rock!");
+
+question(2);
+// Return a string that swaps the case of all letters.
+
+let munstersDescription = "The Munsters are creepy and spooky.";
+
+function caseSwapper(str) {
+  let words = str.split(" ");
+  let modifiedArray = [];
+  words.forEach(word => {
+    let charArray = [...word];
+    let modifiedWord = '';
+    charArray.forEach(char => {
+      if (char === char.toUpperCase()) {
+        modifiedWord += char.toLowerCase();
+      } else {
+        modifiedWord += char.toUpperCase();
+      }
+    });
+    modifiedArray.push(modifiedWord);
+  });
+  let result = modifiedArray.join(" ");
+  console.log(result);
+};
+
+caseSwapper(munstersDescription);
