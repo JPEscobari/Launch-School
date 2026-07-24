35e3a62ddf167dab66bbc6cb1b91fc25b1c0ca19
JP <jpe911@gmail.com>
2026-07-24 07:39:33 -0400

    Finish Sum_or_Product.js Easy 1 Problem

--- JS101-Programming-Foundations/Lesson_3/Small_Problems/Easy_1/Sum_or_Product.js
+++ JS101-Programming-Foundations/Lesson_3/Small_Problems/Easy_1/Sum_or_Product.js
@@ -1,5 +1,54 @@
 const readline = require('readline-sync');
+let result = null;
 
-console.log("Write your answer: ");
-const answer = readline.question();
-console.log(`Hello ${answer}`);
+function prompt(str) {
+  console.log(`=> ${str}`);
+}
+
+function isValidNumber(number) {
+  return Number.isInteger(Number(number)) && Number(number) > 0;
+}
+
+function isValidOperation(char) {
+  return char === 's' || char === 'p';;
+}
+
+prompt("Please enter an integer greater than 0: ");
+let answer = readline.question();
+
+while (!isValidNumber(answer)) {
+  prompt("Number out of range.");
+  prompt("Please enter an integer greater than 0: ");
+  answer = readline.question();
+}
+
+answer = Number(answer);
+
+prompt('Enter "s" to compute the sum, or "p" to compute the product.');
+let operation = readline.question();
+
+while (!isValidOperation(operation)) {
+  prompt("Invalid Choice.");
+  prompt("Please type 's' to compute the sum or 'p' to compute the product: ");
+  operation = readline.question();
+}
+
+switch (operation) {
+  case 's':
+    result = 0;
+    for (let start = 1; start <= answer; start++) {
+      result += start;
+    }
+    console.log(`The sum of the integers between 1 and ${answer} is: ${result}`);
+    break;
+  case 'p':
+    result = 1;
+    for (let start = 1; start <= answer; start++) {
+      result *= start;
+    }
+    console.log(`The product of the integers between 1 and ${answer} is: ${result}`);
+    break;
+
+  default:
+    break;
+}
